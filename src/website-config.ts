e0c85486092cc295de809cf1c3fba516cff8316f
danklut <aomcloudenterprise@outlook.com>
2019-06-10 22:44:34 -0700

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -55,7 +55,7 @@ const config: WebsiteConfig = {
   logo: 'img/ghost-logo.png',
   lang: 'en',
   siteUrl: 'https://aomcloudenterprise.netlify.com',
-  facebook: https://www.facebook.com/AOMCloud/?modal=admin_todo_tourt',
+  facebook: 'https://www.facebook.com/AOMCloud/?modal=admin_todo_tour',
   twitter: 'https://twitter.com/klutmeyer',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
